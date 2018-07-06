9de13e76b07408205314b2685818c44ed92c8557
Adam Kulpa <ad200@gazeta.pl>
2018-07-07 00:47:35 +0100

    Change html, css, js

---
+++ js/App.js
@@ -0,0 +1,28 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': 3141,
+  'X-Auth-Token': 'ce0fd06f05ad3cb5d349003a4193a01d'
+};
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(cardObj);
+  	})
+}
+
